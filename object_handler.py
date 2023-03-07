cf6d97d935b6cfbcf1b6d4290b24b0a8da9d5c3b
Stoia Adrian <stoiaadrian88@gmail.com>
2023-03-07 16:55:23 +0200

    initial commit

---
+++ object_handler.py
@@ -0,0 +1,54 @@
+from sprite_object import *
+from npc import *
+
+
+class ObjectHandler:
+    def __init__(self, game):
+        self.game = game
+        self.sprite_list = []
+        self.npc_list = []
+        self.npc_sprite_list = 'resources/sprites/npc/'
+        self.static_sprite_path = 'resources/sprites/static_sprites/'
+        self.anim_sprite_path = 'resources/sprites/animated_sprites/'
+        add_sprite = self.add_sprite
+        add_npc = self.add_npc
+        self.npc_position = {}
+
+        # sprite map
+        add_sprite(SpriteObject(game))
+        add_sprite(AnimatedSprite(game))
+        add_sprite(AnimatedSprite(game, pos=(1.5, 1.5)))
+        add_sprite(AnimatedSprite(game, pos=(1.5, 7.5)))
+        add_sprite(AnimatedSprite(game, pos=(5.5, 3.25)))
+        add_sprite(AnimatedSprite(game, pos=(5.5, 4.75)))
+        add_sprite(AnimatedSprite(game, pos=(7.5, 2.5)))
+        add_sprite(AnimatedSprite(game, pos=(7.5, 5.5)))
+        add_sprite(AnimatedSprite(game, pos=(14.5, 1.5)))
+        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'red_light/0.png', pos=(14.5, 7.5)))
+        #add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'red_light/0.png', pos=(12.5, 7.5)))
+        add_sprite(AnimatedSprite(game, path=self.anim_sprite_path + 'red_light/0.png', pos=(9.5, 7.5)))
+
+        # npc map
+        add_npc(NPC(game))
+        add_npc(NPC(game, pos=(7.5, 1.5)))
+        add_npc(NPC(game, pos=(9.5, 7.5)))
+        add_npc(NPC(game, pos=(10.5, 3.25)))
+
+    def check_win(self):
+        if not len(self.npc_position):
+            self.game.object_renderer.win()
+            pg.display.flip()
+            pg.time.delay(1500)
+            self.game.new_game
+
+    def update(self):
+        self.npc_position = {npc.map_pos for npc in self.npc_list if npc.alive}
+        [sprite.update() for sprite in self.sprite_list]
+        [npc.update() for npc in self.npc_list]
+        self.check_win()
+
+    def add_npc(self, npc):
+        self.npc_list.append(npc)
+
+    def add_sprite(self, sprite):
+        self.sprite_list.append(sprite)
