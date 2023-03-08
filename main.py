e0fa1463f747d52ce3f7d8ab76f77974c20b153f
Stoia Adrian <stoiaadrian88@gmail.com>
2023-03-08 14:24:49 +0200

    Fixing the bug

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Game:
     def __init__(self):
         pg.init()
         pg.mouse.set_visible(False)
-        self.screen = pg.display.set_mode((WIDTH, HEIGTH))
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         self.clock = pg.time.Clock()
         self.delta_time = 1
         self.new_game()
